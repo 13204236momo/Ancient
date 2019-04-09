5b61f77ddd51f2c46f93c94cd05181359842b136
xiaomo <953605663@qq.com>
2019-04-09 17:04:34 +0800

    完善绘制

---
+++ app/src/main/java/com/example/zhoumohan/ancient/util/BitmapUtil.java
@@ -0,0 +1,31 @@
+package com.example.zhoumohan.ancient.util;
+
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
+public class BitmapUtil {
+
+    /**
+     * 根据指定宽高缩放
+     * @param origin
+     * @param newWidth
+     * @param newHeight
+     * @return
+     */
+    public static Bitmap scaleBitmap(Bitmap origin, int newWidth, int newHeight) {
+        if (origin == null) {
+            return null;
+        }
+        int height = origin.getHeight();
+        int width = origin.getWidth();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);// 使用后乘
+        Bitmap newBM = Bitmap.createBitmap(origin, 0, 0, width, height, matrix, false);
+        if (!origin.isRecycled()) {
+            origin.recycle();
+        }
+        return newBM;
+    }
+}
